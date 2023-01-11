d505284c110232cee6b44213b1ce35c8f090f699
xingxiang <xingxiang@twotreesgroup.com>
2023-01-11 20:05:00 +0800

    renamed VERCEL_URL to NEXT_PUBLIC_VERCEL_URL

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -14,7 +14,8 @@ import { type AppRouter } from "../server/api/root";
 
 export const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  if (process.env.NEXT_PUBLIC_VERCEL_URL)
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
--- src/utils/link.ts
+++ src/utils/link.ts
@@ -1,4 +1,5 @@
 export const getFullBaseUrl = () => {
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  if (process.env.NEXT_PUBLIC_VERCEL_URL)
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
