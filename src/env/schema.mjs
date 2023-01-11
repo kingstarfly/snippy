f15c8e0846790118d13980449682fdeb63ac5480
xingxiang <xingxiang@twotreesgroup.com>
2023-01-11 09:06:11 +0800

    added vercel url as env var

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -17,6 +17,7 @@ export const serverSchema = z.object({
  */
 export const clientSchema = z.object({
   // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  NEXT_PUBLIC_VERCEL_URL: z.string().url(),
 });
 
 /**
@@ -27,4 +28,5 @@ export const clientSchema = z.object({
  */
 export const clientEnv = {
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
 };
