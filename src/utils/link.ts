0a4f9514b7fc82b6aef1b533929000ee8512a2ea
xingxiang <xingxiang@twotreesgroup.com>
2023-01-11 20:09:19 +0800

    removed duplicate https://

--- src/utils/link.ts
+++ src/utils/link.ts
@@ -1,5 +1,5 @@
 export const getFullBaseUrl = () => {
   if (process.env.NEXT_PUBLIC_VERCEL_URL)
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`; // SSR should use vercel url
+    return `${process.env.NEXT_PUBLIC_VERCEL_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
